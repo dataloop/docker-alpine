cf9a2b18d20fe38ae62455ccaa506a4f55b85926
Thibault Laurens <laurens.thibault@gmail.com>
2017-02-01 18:17:07 +0000

    discover.py without cadvisor

--- dataloop-docker/root/opt/dataloop/embedded/bin/discover.py
+++ dataloop-docker/root/opt/dataloop/embedded/bin/discover.py
@@ -7,32 +7,28 @@ import dl_lib
 import os
 
 logger = logging.getLogger('DISCOVER')
-
 os.environ['NO_PROXY'] = '127.0.0.1'
 
 
 def registration(ctx):
+    logger.info("register sync")
+
     try:
         register_sync(ctx)
     except Exception as ex:
         logger.error("register sync failed: %s" % ex, exc_info=True)
-    finally:
-        time.sleep(ctx['register_interval'])
 
 
 def register_sync(ctx):
-        logger.info("register sync")
         agents = dl_lib.get_agents(ctx)
         agent_ids = dl_lib.get_agents_ids(agents)
-        containers = dl_lib.get_containers(ctx)
-        container_hashes = get_container_hashes(containers)
-        logger.debug("containers: %s", container_hashes)
+
+        containers = dl_lib.get_containers()
+        container_hashes = dl_lib.get_container_hashes(containers)
+
+        logger.debug("agent_ids=%s container_hashes=%s", agent_ids, container_hashes)
         dead_containers = agent_ids - container_hashes
         destroy_agents(ctx, dead_containers)
-
-
-def get_container_hashes(containers):
-    return set(map(dl_lib.hash_id, dl_lib.get_container_paths(containers)))
 
 
 def destroy_agents(ctx, agent_ids):
@@ -40,7 +36,7 @@ def destroy_agents(ctx, agent_ids):
     headers = dl_lib.get_request_headers(ctx)
 
     def create_request(id):
-        url = "%s/agents/%s/deregister" % (api_host, id,)
+        url = "%s/agents/%s/deregister" % (api_host, id)
         return grequests.post(url, headers=headers)
 
     reqs = map(create_request, agent_ids)
@@ -51,28 +47,42 @@ def main(argv):
     ctx = {
         "register_interval": 30,
         "api_host": "https://agent.dataloop.io",
-        "cadvisor_host": "http://127.0.0.1:8080"
+        "api_key": None
     }
 
     try:
-        opts, args = getopt.getopt(argv, "ha:c:u::", ["apikey=", "cadvisor=", "apiurl="])
-    except getopt.GetoptError:
-        print 'discover.py -a <apikey> -c <cadvisor address:port> -u <dataloop api address:port>'
+        opts, args = getopt.getopt(argv, "ha:u:", ["help", "apikey=", "apiurl="])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
         sys.exit(2)
+
     for opt, arg in opts:
-        if opt == '-h':
-            print 'discover.py -a <apikey> -c <cadvisor address:port> -u <dataloop address:port>'
-            sys.exit()
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit(2)
         elif opt in ("-a", "--apikey"):
             ctx['api_key'] = arg
-        elif opt in ("-c", "--cadvisor"):
-            ctx['cadvisor_host'] = arg
         elif opt in ("-u", "--apiurl"):
             ctx['api_host'] = arg
+
+    if ctx['api_key'] is None:
+        usage()
+        sys.exit(2)
 
     while True:
         registration(ctx)
         time.sleep(ctx['register_interval'])
+
+
+def usage():
+    usage = """discover.py
+    -h --help                   Prints this
+    -a --apikey <apikey>        Your dataloop api key
+    -u --apiurl <apiurl>        The dataloop api url (default to "https://agent.dataloop.io")
+    """
+    print usage
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
